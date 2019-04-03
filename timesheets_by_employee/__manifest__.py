41d5610e44b7afa4bf8adf2a4570ab7aa5450f43
atomxSylvein <sylvein.faraux@atomxsystem.com>
2019-04-03 14:53:23 +0200

    màj du manifest

--- timesheets_by_employee/__manifest__.py
+++ timesheets_by_employee/__manifest__.py
@@ -21,14 +21,14 @@
 ##############################################################################
 {
     'name': 'Timesheet PDF Report',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.0.1',
     "category": "Generic Modules/Human Resources",
     'sequence': 25,
     'summary': 'Timesheet PDF Report of Employees',
     'category': "Generic Modules/Human Resources",
     'company': 'Cybrosys Techno Solutions',
     'website': 'www.cybrosys.com',
-    'depends': ['hr_timesheet_sheet'],
+    'depends': ['timesheet_grid'],
     'data': [
              'report/report_timesheets.xml',
              'wizard/timesheet_wizard.xml',
